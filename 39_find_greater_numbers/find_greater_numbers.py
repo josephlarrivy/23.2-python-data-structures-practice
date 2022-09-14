3294cd7d64e21c8e6fecf1d000ea2c48331b49db
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-14 16:03:58 -0500

    exercises complete

--- 39_find_greater_numbers/find_greater_numbers.py
+++ 39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,23 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    # my work
+    # counter = 0
+    # for i in range(len(nums)):
+    #     print(i)
+    #     if nums[i] < nums[i+1]:
+    #         counter = counter +1
+    # return counter 
+
+
+
+    solution
+
+    count = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] > nums[i]:
+                count += 1
+
+    return count
